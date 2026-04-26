bbc940b7283338c95bb296cc960ac4f3cab017e9
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-04-26 23:37:33 +0600

    navigation bar and set theme in the website

---
+++ src/components/ThemeSwitch.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { useTheme } from "next-themes";
+
+
+export function ThemeSwitch() {
+    const { theme, setTheme } = useTheme();
+    return (
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            Toggle {theme === "dark" ? "Light" : "Dark"} Mode
+        </button>
+    );
+}
