b7fa591a0109b9e2580e3f8aa5cb56d1dd891a87
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-04-27 19:51:11 +0600

    creating first server action and send from data to the server action

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { CirclePlus } from "@gravity-ui/icons";
-import { Button, Input, Label, Modal, Surface, TextField } from "@heroui/react";
+import { Button, Input, Label, ListBox, Modal, Surface, TextField, Select } from "@heroui/react";
 
-export function AddTask({createATask}) {
+export function AddTask({ createATask }) {
   return (
     <Modal>
       <Button variant="secondary">Add a Task</Button>
@@ -20,25 +20,68 @@ export function AddTask({createATask}) {
             <Modal.Body className="p-6">
               <Surface variant="default">
                 <form action={createATask} className="flex flex-col gap-4">
-                  <TextField className="w-full" name="name" type="text">
-                    <Label>Name</Label>
-                    <Input placeholder="Enter your name" />
+                  <TextField className="w-full" name="title" type="text">
+                    <Label>Title</Label>
+                    <Input placeholder="Enter your Task Title" />
                   </TextField>
-                  <TextField className="w-full" name="email" type="email">
-                    <Label>Email</Label>
-                    <Input placeholder="Enter your email" />
+                  <TextField className="w-full" name="description" type="text">
+                    <Label>Description</Label>
+                    <Input placeholder="Enter your Task Description" />
                   </TextField>
-                  <TextField className="w-full" name="phone" type="tel">
-                    <Label>Phone</Label>
-                    <Input placeholder="Enter your phone number" />
-                  </TextField>
-                  <TextField className="w-full" name="company">
-                    <Label>Company</Label>
-                    <Input placeholder="Enter your company name" />
-                  </TextField>
-                  <TextField className="w-full" name="message">
-                    <Label>Message</Label>
-                    <Input placeholder="Enter your message" />
+
+                  {/* Select Status */}
+                  <Select name="status" className="w-[256px]" placeholder="Select one">
+                    <Label>Status</Label>
+                    <Select.Trigger>
+                      <Select.Value />
+                      <Select.Indicator />
+                    </Select.Trigger>
+                    <Select.Popover>
+                      <ListBox>
+                        <ListBox.Item id="pending" textValue="Pending">
+                          Pending
+                          <ListBox.ItemIndicator />
+                        </ListBox.Item>
+                        <ListBox.Item id="in-progress" textValue="In Progress">
+                          In Progress
+                          <ListBox.ItemIndicator />
+                        </ListBox.Item>
+                        <ListBox.Item id="completed" textValue="Completed">
+                          Completed
+                          <ListBox.ItemIndicator />
+                        </ListBox.Item>
+                      </ListBox>
+                    </Select.Popover>
+                  </Select>
+
+                  {/* Select Priority */}
+                  <Select name="priority" className="w-[256px]" placeholder="Select one">
+                    <Label>Priority</Label>
+                    <Select.Trigger>
+                      <Select.Value />
+                      <Select.Indicator />
+                    </Select.Trigger>
+                    <Select.Popover>
+                      <ListBox>
+                        <ListBox.Item id="low" textValue="Low">
+                          Low
+                          <ListBox.ItemIndicator />
+                        </ListBox.Item>
+                        <ListBox.Item id="medium" textValue="Medium">
+                          Medium
+                          <ListBox.ItemIndicator />
+                        </ListBox.Item>
+                        <ListBox.Item id="high" textValue="High">
+                          High
+                          <ListBox.ItemIndicator />
+                        </ListBox.Item>
+                      </ListBox>
+                    </Select.Popover>
+                  </Select>
+
+                  <TextField className="w-full" name="username">
+                    <Label>Username</Label>
+                    <Input placeholder="Enter your username" />
                   </TextField>
                   <Modal.Footer>
                     <Button slot="close" variant="secondary">
